47cc4ec1533753819d424c8d23cd64162d65f2d4
Mustafa Ali <engr.mustafa.ali.mir@gmail.com>
2021-10-29 11:11:53 +0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,13 @@
 mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"engr.mustafa.ali.mir@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
 echo "\
 [server]\n\
 headless = true\n\
+enableCORS=false\n\
 port = $PORT\n\
-enableCORS = false\n\
-\n\
 " > ~/.streamlit/config.toml
